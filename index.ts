2fc38839a9045785862f545354315eaa8ce9ecd4
Lucas De Curtis <lucas.de-curtis@epitech.eu>
2023-06-21 21:22:29 +0900

    first commit
    Signed-off-by: Lucas De Curtis <lucas.de-curtis@epitech.eu>

---
+++ index.ts
@@ -0,0 +1,3 @@
+export const MISSING_FIELDS = "Un ou plusieurs champs sont manquants";
+export const ERROR_OCCURED = "Une erreur est survenue";
+export const REQUIRED_FIELD = "Ce champs est requis";
